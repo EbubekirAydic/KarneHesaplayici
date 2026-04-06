9ec1534bb254527d72bb659cda4e52d9b1746675
Ebubekir Aydıç <ebuaydic2@gmail.com>
2026-04-06 22:24:00 +0300

    2026 Update
    - CSS update
    - Küçültülmüş ana menü
    - JS update
    - Küçük hatalar düzeltildi
    - DAHA YAPILACAK ÇOK ŞEY VAR

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+
 document.getElementById("dersEkle").addEventListener("click", () => {
     console.log("➕ Yeni ders ekleniyor...");
     dersEkle("");
+    IWInputs = document.querySelectorAll('.IW');
 });
 
 document.getElementById("derslerEkle").addEventListener("click", () => {
     console.log("➕ Yeni dersler ekleniyor...");
     derslerEkle(document.getElementById("derslerInput").value);
+    // IWInput'u verilerini güncelle
+    IWInputs = document.querySelectorAll('.IW');
 });
 
 document.getElementById("hesapla").addEventListener("click", () => {
@@ -33,13 +37,13 @@ veridenYukle();
 function dersEkle(ad = "", donem1 = ["","","",""], donem2 = ["","","",""], saat = "") {
     console.log("✏️ Ders ekleme fonksiyonu çalıştı:", ad, donem1, donem2, saat);
     
-    let dersKapsayici = document.createElement("div");
-    dersKapsayici.classList.add("ders");
+    let dersDiv = document.createElement("div");
+    dersDiv.classList.add("ders");
 
-    dersKapsayici.innerHTML = `
+    dersDiv.innerHTML = `
     <div class="dersBilgi">
-            <input type="text" placeholder="Ders Adı" class="dersAdi" value="${ad}">
-            <p>(<input type="number" class="dersAdi haftalikSaat HDS" placeholder="HDS" value="${saat}">Saat)</p>
+            <input type="text" placeholder="Ders Adı" class="dersAdi IW" value="${ad? ad : ""}">
+            <p>(<input type="number" min="0" max="9" class="dersAdi haftalikSaat IW" placeholder="HDS" value="${saat? saat : ""}" oninput="this.value = Math.floor(this.value); if(this.value > 9) this.value = 9; if(this.value < 0) this.value = 0;">Saat)</p>
         </div>
         <h3>1. Dönem</h3>
         <div class="notlar">
@@ -49,13 +53,13 @@ function dersEkle(ad = "", donem1 = ["","","",""], donem2 = ["","","",""], saat 
                 <p class="isimTable">3.</p>
 
                 <p class="isimTable">Sınav</p>
-                <p class="nots"><input type="number" class="puan donem1" placeholder="1. Sınav" value="${donem1[0]}"></p>
-                <p class="nots"><input type="number" class="puan donem1" placeholder="2. Sınav" value="${donem1[1]}"></p>
+                <p class="nots"><input type="number" class="puan donem1" placeholder="1. Sınav" value="${donem1[0]? donem1[0] : ""}"></p>
+                <p class="nots"><input type="number" class="puan donem1" placeholder="2. Sınav" value="${donem1[1]? donem1[1] : ""}"></p>
                 <p class="nots"><input type="number" class="puan" placeholder="" value=""></p>
 
                 <p class="isimTable">Sözlü</p>
-                <p class="nots"><input type="number" class="puan donem1" placeholder="1. Sözlü" value="${donem1[2]}"></p>
-                <p class="nots"><input type="number" class="puan donem1" placeholder="2. Sözlü" value="${donem1[3]}"></p>
+                <p class="nots"><input type="number" class="puan donem1" placeholder="1. Sözlü" value="${donem1[2]? donem1[2] : ""}"></p>
+                <p class="nots"><input type="number" class="puan donem1" placeholder="2. Sözlü" value="${donem1[3]? donem1[3] : ""}"></p>
                 <p class="nots"><input type="number" class="puan" placeholder="" value=""></p>
 
                 <p></p>
@@ -71,13 +75,13 @@ function dersEkle(ad = "", donem1 = ["","","",""], donem2 = ["","","",""], saat 
                 <p class="isimTable">3.</p>
 
                 <p class="isimTable">Sınav</p>
-                <p class="nots"><input type="number" class="puan donem2" placeholder="1. Sınav" value="${donem2[0]}"></p>
-                <p class="nots"><input type="number" class="puan donem2" placeholder="2. Sınav" value="${donem2[1]}"></p>
+                <p class="nots"><input type="number" class="puan donem2" placeholder="1. Sınav" value="${donem2[0]? donem2[0] : ""}"></p>
+                <p class="nots"><input type="number" class="puan donem2" placeholder="2. Sınav" value="${donem2[1]? donem2[1] : ""}"></p>
                 <p class="nots"><input type="number" class="puan" placeholder="" value=""></p>
 
                 <p class="isimTable">Sözlü</p>
-                <p class="nots"><input type="number" class="puan donem2" placeholder="1. Sözlü" value="${donem2[2]}"></p>
-                <p class="nots"><input type="number" class="puan donem2" placeholder="2. Sözlü" value="${donem2[3]}"></p>
+                <p class="nots"><input type="number" class="puan donem2" placeholder="1. Sözlü" value="${donem2[2]? donem2[2] : ""}"></p>
+                <p class="nots"><input type="number" class="puan donem2" placeholder="2. Sözlü" value="${donem2[3]? donem2[3] : ""}"></p>
                 <p class="nots"><input type="number" class="puan" placeholder="" value=""></p>
 
                 <p></p>
@@ -88,42 +92,41 @@ function dersEkle(ad = "", donem1 = ["","","",""], donem2 = ["","","",""], saat 
         <br>
         <div class="notlar">
                 <p></p>
-                <p></p>
+                <p><button class="dersKaldir">Kaldır</button></p>
                 <p class="isimTable">Toplam Puanı</p>
                 <p class="notsOrt ortalama">100</p>
         </div>
-
-        <button class="dersKaldir">Kaldır</button>
         <hr>
     `;
 
-    document.getElementById("dersler").appendChild(dersKapsayici);
+    document.getElementById("dersler").appendChild(dersDiv);
 
-    dersKapsayici.querySelectorAll("input").forEach(input => {
+    dersDiv.querySelectorAll("input").forEach(input => {
         input.addEventListener("input", () => {
+            console.log("--------------------------------");
             console.log("⌨️ Input değeri değişti, ortalama güncelleniyor ve veri kaydediliyor...");
-            dersOrtalamaGuncelle(dersKapsayici);
+            dersOrtalamaGuncelle(dersDiv);
             veriyiKaydet();
         });
     });
 
-    dersKapsayici.querySelector(".dersKaldir").addEventListener("click", () => {
+    dersDiv.querySelector(".dersKaldir").addEventListener("click", () => {
         console.log("🗑️ Ders kaldırılıyor...");
-        dersKapsayici.remove();
+        dersDiv.remove();
         veriyiKaydet();
     });
 
-    dersOrtalamaGuncelle(dersKapsayici);
+    dersOrtalamaGuncelle(dersDiv);
     veriyiKaydet();
 }
 
-function dersOrtalamaGuncelle(dersKapsayici) {
+function dersOrtalamaGuncelle(dersDiv) {
     console.log("🔄 Ders ortalaması güncelleniyor...");
-    const puanlarDonem1 = [...dersKapsayici.querySelectorAll(".donem1")]
+    const puanlarDonem1 = [...dersDiv.querySelectorAll(".donem1")]
         .map(input => input.value !== "" ? Number(input.value) : null)
         .filter(puan => puan !== null); // Boş inputları filtrele
 
-    const puanlarDonem2 = [...dersKapsayici.querySelectorAll(".donem2")]
+    const puanlarDonem2 = [...dersDiv.querySelectorAll(".donem2")]
         .map(input => input.value !== "" ? Number(input.value) : null)
         .filter(puan => puan !== null); // Boş inputları filtrele
 
@@ -141,10 +144,10 @@ function dersOrtalamaGuncelle(dersKapsayici) {
         toplamOrtalama = ""; // Eğer hiç not girilmediyse
     }
 
-    dersKapsayici.querySelector(".ortalama1").innerText = ortDonem1 ? `${ortDonem1}` : 'Not Girilmedi';
-    dersKapsayici.querySelector(".ortalama2").innerText = ortDonem2 ? `${ortDonem2}` : 'Not Girilmedi';
+    dersDiv.querySelector(".ortalama1").innerText = ortDonem1 ? `${ortDonem1}` : '-';
+    dersDiv.querySelector(".ortalama2").innerText = ortDonem2 ? `${ortDonem2}` : '-';
     
-    dersKapsayici.querySelector(".ortalama").innerText = toplamOrtalama ? `${toplamOrtalama}` : 'Not Girilmedi';
+    dersDiv.querySelector(".ortalama").innerText = toplamOrtalama ? `${toplamOrtalama}` : '-';
     console.log("📊 Ortalama güncellendi:", toplamOrtalama);
 }
 
@@ -198,17 +201,19 @@ function karneHesapla() {
         }
     });
 
-    const donem1Ortalama = toplamSaatDonem1 > 0 ? Number((toplamPuanDonem1 / toplamSaatDonem1).toFixed(2)) : "Not Girilmedi";
-    const donem2Ortalama = toplamSaatDonem2 > 0 ? Number((toplamPuanDonem2 / toplamSaatDonem2).toFixed(2)) : "Not Girilmedi";
+    const donem1Ortalama = toplamSaatDonem1 > 0 ? Number((toplamPuanDonem1 / toplamSaatDonem1).toFixed(2)) : "-";
+    const donem2Ortalama = toplamSaatDonem2 > 0 ? Number((toplamPuanDonem2 / toplamSaatDonem2).toFixed(2)) : "-";
+    const donem1OrtalamaN = toplamSaatDonem1 > 0 ? Number((toplamPuanDonem1 / toplamSaatDonem1).toFixed(2)) : 0;
+    const donem2OrtalamaN = toplamSaatDonem2 > 0 ? Number((toplamPuanDonem2 / toplamSaatDonem2).toFixed(2)) : 0;
 
     console.log(donem1Ortalama, ' + ', donem2Ortalama, ' = ', (donem1Ortalama + donem2Ortalama),'\n',(donem1Ortalama + donem2Ortalama) / 2);
 
-    const genelOrtalama = (donem1Ortalama + donem2Ortalama) / 2;
+    const genelOrtalama = (donem1OrtalamaN + donem2OrtalamaN) / 2;
 
     document.getElementById("sonuc").innerHTML = `
-        Karne Ortalaması: ${genelOrtalama} <br>
         1. Dönem Ortalaması: ${donem1Ortalama} <br>
-        2. Dönem Ortalaması: ${donem2Ortalama}
+        2. Dönem Ortalaması: ${donem2Ortalama} <br>
+        Karne Ortalaması: ${genelOrtalama}
     `;
     console.log("🎉 Karne ortalaması hesaplandı:", genelOrtalama);
     console.log("🎉 1. Dönem ortalaması hesaplandı:", donem1Ortalama);
@@ -258,3 +263,51 @@ function derslerEkle(dersler) {
 
 
 document.getElementById('bilgi').innerHTML = localStorage.getItem("dersler");
+
+
+
+// Gizli bir ölçücü oluşturma fonksiyonu
+function createMeasurer(input) {
+    const span = document.createElement('span');
+    span.style.visibility = 'hidden';
+    span.style.position = 'absolute';
+    span.style.whiteSpace = 'pre'; // Boşlukları korumak için
+    
+    // Input'un tüm yazı özelliklerini span'a kopyala
+    const style = window.getComputedStyle(input);
+    span.style.fontFamily = style.fontFamily;
+    span.style.fontSize = style.fontSize;
+    span.style.fontWeight = style.fontWeight;
+    span.style.letterSpacing = style.letterSpacing;
+    span.style.textTransform = style.textTransform;
+    
+    document.body.appendChild(span);
+    return span;
+}
+
+let IWInputs = document.querySelectorAll('.IW');
+IWInputs.forEach(input => {
+    console.log("???????????????????????????????????????");
+    const measurer = createMeasurer(input);
+
+    const updateWidth = () => {
+        // Eğer değer boşsa placeholder'ı ölç, doluysa değeri
+        measurer.textContent = input.value || input.placeholder || "";
+        console.log(measurer.textContent);
+        
+        // Span'ın genişliğini al ve input'a uygula (birkaç piksel pay bırakmak iyidir)
+        const width = measurer.offsetWidth;
+        console.log(width);
+        input.style.width = (width + 2) + "px"; // 12px pay, padding/border için
+    };
+
+    // İlk yüklemede ayarla
+    updateWidth();
+
+    // Yazı yazıldıkça güncelle
+    input.addEventListener('input', () => {
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        updateWidth();
+    });
+});
+console.log(IWInputs);
